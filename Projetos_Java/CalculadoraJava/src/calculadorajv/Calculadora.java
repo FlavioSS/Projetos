b301cd705f0915986a80bc6ca81d14788cc22d25
FlavioSS <54990626+FlavioSS@users.noreply.github.com>
2019-10-01 20:50:16 -0300

    Versão 4 da Calculadora
    Submétodos para retorno das operações acessando diretamente o objeto do método estático calculadora.

--- Projetos_Java/CalculadoraJava/src/calculadorajv/Calculadora.java
+++ Projetos_Java/CalculadoraJava/src/calculadorajv/Calculadora.java
@@ -3,65 +3,74 @@ package calculadorajv;
 import javax.swing.JOptionPane;
 
 public class Calculadora {
-
-    public static void main(String args[]) {
-
-        String v1;
-        String v2;
-        String opcao;
-        double resultadofinal = 0D;
-        String msg;
-
-        opcao = JOptionPane.showInputDialog(
-                "Informe a operação: \n1 - Soma;" + " \n2 - Subtração; "
-                        + "\n3 - Multiplicação " + "\n4 - Divisão.");
-
-        int opc;
-        opc = Integer.parseInt(opcao);
-        v1 = JOptionPane.showInputDialog("Informe o primeiro valor: ");
-        v2 = JOptionPane.showInputDialog("Informe o segundo valor: ");
-        final double valor1 = Double.parseDouble(v1);
-        final double valor2 = Double.parseDouble(v2);
-
-        if (opc == 1) {
-            resultadofinal = valor1 + valor2;
-        } else if (opc == 2) {
-            resultadofinal = valor1 - valor2;
-        } else if (opc == 3) {
-            resultadofinal = valor1 * valor2;
-        } else if (opc == 4) {
-            resultadofinal = valor1 / valor2;
-        }
-
-        msg = "O Resultado Final é: " + resultadofinal;
-        JOptionPane.showMessageDialog(null, msg);
-
-    }
-    
+	static int opc;
 
 
-    	public double add(double valor1, double valor2) {
+	public static void main(String args[]) {
+		Calculadora calc = new Calculadora();
+		String v1;
+		String v2;
+		String opcao;
+		double resultadofinal = 0D;
+		String msg;
 
-    		return valor1 + valor2;
+		opcao = JOptionPane.showInputDialog(
+				"Informe a operação: \n1 - Soma;" + " \n2 - Subtração; "
+						+ "\n3 - Multiplicação " + "\n4 - Divisão.");
 
-    	}
 
-    	public double sub(double valor1, double valor2) {
+		opc = Integer.parseInt(opcao);
+		v1 = JOptionPane.showInputDialog("Informe o primeiro valor: ");
+		v2 = JOptionPane.showInputDialog("Informe o segundo valor: ");
+		final double valor1 = Double.parseDouble(v1);
+		final double valor2 = Double.parseDouble(v2);
 
-    		return valor1 - valor2;
+		if (opc == 1) {
+			resultadofinal = calc.some(valor1,valor2);
+		} else if (opc == 2) {
+			resultadofinal = calc.sub(valor1,valor2);
+		} else if (opc == 3) {
+			resultadofinal = calc.multi(valor1,valor2);
+		} else if (opc == 4) {
+			resultadofinal = calc.div(valor1,valor2);
+		}
 
-    	}
+		msg = "O Resultado Final é: " + resultadofinal;
+		JOptionPane.showMessageDialog(null, msg);
 
-    	public double multi(double valor1, double valor2) {
+	}
 
-    		return valor1 * valor2;
 
-    	}
+		public double some(double valor1, double valor2) {
+			double resultar = 0D;
+				if (opc == 1) {
+					resultar = valor1 + valor2;
+				}
+		return resultar;
+		}
 
-    	public double div(double valor1, double valor2) {
+		public double sub(double valor1, double valor2) {
+			double resultar = 0D;
+				if (opc == 2) {
+					return valor1 - valor2;
+				}
+			return resultar;
+		}
 
-    		return valor1 / valor2;
+		public double multi(double valor1, double valor2) {
+			double resultar = 0D;
+				if (opc == 3) {
+					return valor1 * valor2;
+				}
+			return resultar;
+		}
 
-    	}
+		public double div(double valor1, double valor2) {
+			double resultar = 0D;
+				if (opc == 4) {
+					return valor1 / valor2;
+				}
+				return resultar;
+		}
 
 }
