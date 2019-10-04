54ad8001da35f9fd0b0b600b2bf94f325e7bf023
FlavioSS <54990626+FlavioSS@users.noreply.github.com>
2019-10-04 18:00:25 -0300

    Versão 5 da calculadora
    Retirada de método new calc duplicado. Acessando mais diretamente o método estático da classe. Retirada de if dos submétodos para retornar direto. Alinhamento de todos os métodos como estáticos.

--- Projetos_Java/CalculadoraJava/src/calculadorajv/Calculadora.java
+++ Projetos_Java/CalculadoraJava/src/calculadorajv/Calculadora.java
@@ -3,74 +3,75 @@ package calculadorajv;
 import javax.swing.JOptionPane;
 
 public class Calculadora {
-	static int opc;
 
+    static int opc;
 
-	public static void main(String args[]) {
-		Calculadora calc = new Calculadora();
-		String v1;
-		String v2;
-		String opcao;
-		double resultadofinal = 0D;
-		String msg;
+    public static void main(String args[]) {
 
-		opcao = JOptionPane.showInputDialog(
-				"Informe a operação: \n1 - Soma;" + " \n2 - Subtração; "
-						+ "\n3 - Multiplicação " + "\n4 - Divisão.");
+        String v1;
+        String v2;
+        String opcao;
+        double resultadofinal = 0D;
+        String msg;
 
+        opcao = JOptionPane.showInputDialog(
+                "Informe a operação: \n1 - Soma;" + " \n2 - Subtração; "
+                        + "\n3 - Multiplicação " + "\n4 - Divisão.");
 
-		opc = Integer.parseInt(opcao);
-		v1 = JOptionPane.showInputDialog("Informe o primeiro valor: ");
-		v2 = JOptionPane.showInputDialog("Informe o segundo valor: ");
-		final double valor1 = Double.parseDouble(v1);
-		final double valor2 = Double.parseDouble(v2);
+        opc = Integer.parseInt(opcao);
+        v1 = JOptionPane.showInputDialog("Informe o primeiro valor: ");
+        v2 = JOptionPane.showInputDialog("Informe o segundo valor: ");
+        final double valor1 = Double.parseDouble(v1);
+        final double valor2 = Double.parseDouble(v2);
 
-		if (opc == 1) {
-			resultadofinal = calc.some(valor1,valor2);
-		} else if (opc == 2) {
-			resultadofinal = calc.sub(valor1,valor2);
-		} else if (opc == 3) {
-			resultadofinal = calc.multi(valor1,valor2);
-		} else if (opc == 4) {
-			resultadofinal = calc.div(valor1,valor2);
-		}
+        if (opc == 1) {
+            resultadofinal = Calculadora.some(valor1, valor2);
+        } else if (opc == 2) {
+            resultadofinal = Calculadora.sub(valor1, valor2);
+        } else if (opc == 3) {
+            resultadofinal = Calculadora.multi(valor1, valor2);
 
-		msg = "O Resultado Final é: " + resultadofinal;
-		JOptionPane.showMessageDialog(null, msg);
+        } else if (opc == 4) {
+            resultadofinal = Calculadora.div(valor1, valor2);
+        }
 
-	}
+        msg = "O Resultado Final é: " + resultadofinal;
+        JOptionPane.showMessageDialog(null, msg);
 
+    }
 
-		public double some(double valor1, double valor2) {
-			double resultar = 0D;
-				if (opc == 1) {
-					resultar = valor1 + valor2;
-				}
-		return resultar;
-		}
+    public static double some(double valor1, double valor2) {
+        double resultar = 0D;
 
-		public double sub(double valor1, double valor2) {
-			double resultar = 0D;
-				if (opc == 2) {
-					return valor1 - valor2;
-				}
-			return resultar;
-		}
+        resultar = valor1 + valor2;
 
-		public double multi(double valor1, double valor2) {
-			double resultar = 0D;
-				if (opc == 3) {
-					return valor1 * valor2;
-				}
-			return resultar;
-		}
+        return resultar;
+    }
 
-		public double div(double valor1, double valor2) {
-			double resultar = 0D;
-				if (opc == 4) {
-					return valor1 / valor2;
-				}
-				return resultar;
-		}
+    public static double sub(double valor1, double valor2) {
+        double resultar = 0D;
+
+        resultar = valor1 - valor2;
+
+        return resultar;
+    }
+
+    public static double multi(double valor1, double valor2) {
+        double resultar = 0D;
+
+        resultar = valor1 * valor2;
+
+        return resultar;
+    }
+
+    public static double div(double valor1, double valor2) {
+
+        double resultar = 0D;
+
+        resultar = valor1 / valor2;
+
+        return resultar;
+
+    }
 
 }
